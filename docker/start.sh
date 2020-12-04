573cbf0ff96089db7293973e779cc0bab93e647b
root <you@example.com>
2020-12-04 00:00:47 -0800

    merge

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,8 @@ docker run -it \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="/home/arclabdl1/Linjun/catkin_ws:/root/catkin_ws" \
+    --volume="/home/lilinjun/Programs:/Programs" \
+    --volume="/home/lilinjun/catkin_ws:/root/catkin_ws" \
     linjun/voxblox_omplapp \
     bash
 
